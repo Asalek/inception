0993e0ee8fbea9cc327fe2dbfee181d508620a38
Ayoub Salek <asalek@e2r2p7.1337.ma>
2023-04-18 01:21:47 +0000

    fix: wordpress and mariadb connections and host the worpress index.php file on nginx

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-sed -i "s/bind-address.*/bind-address=${ip_all}/" /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i "s/#port.*/port=${maria_port}/" /etc/mysql/mariadb.conf.d/50-server.cnf
+# sed -i "s/bind-address.*/bind-address=${ip_all}/" /etc/mysql/mariadb.conf.d/50-server.cnf
+# sed -i "s/#port.*/port=${maria_port}/" /etc/mysql/mariadb.conf.d/50-server.cnf
 
 #-i :  modify the file in place
 #s/MY_VARIABLE=.*/MY_VARIABLE=${new_value}/     : matches any line in the file that starts with MY_VARIABLE= and replaces the entire line with MY_VARIABLE=new_value.
 
 service mysql start
-sleep 5;
 
 touch db
 
 echo "CREATE DATABASE $db_name;" >> db
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$db_root_passwd';" >> db
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '1234';" >> db
 echo "CREATE USER '$db_user'@'%' IDENTIFIED BY '$db_passwd';" >> db
 echo "GRANT ALL PRIVILEGES ON *.* TO '$db_user'@'%';" >> db
 echo "FLUSH PRIVILEGES;" >> db
@@ -27,7 +26,8 @@ kill $(cat /var/run/mysqld/mysqld.pid)
 #mysqld_safe will attempt to restart it automatically.
 #if there is already a running instance of the MySQL server (as indicated by the PID file),
 #running mysqld_safe will likely fail, because it cannot start a new instance of the server while there is already one running.
-sleep 5;
+
 #The *.* wildcard notation implies that the user has permission to execute any task on any database in the database server.
 
-/usr/bin/mysqld_safe
+# /usr/bin/mysqld_safe
+mysqld
--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -10,9 +10,15 @@ chmod 777 wp-config.php
 sed -i "s/database_name_here/$db_name/1" wp-config.php
 sed -i "s/password_here/$db_passwd/1" wp-config.php
 sed -i "s/username_here/$db_user/1" wp-config.php
-# sed -i "s/localhost/mariadb/1" wp-config.php
+sed -i "s/localhost/mariadb/1" wp-config.php
 
-# wp user create $wp_admin $wp_admin_email user_pass=$wp_admin_psswd --role=administrator --porcelain
+wp core install --title=$wp_title \
+                --admin_user=$wp_admin \
+                --admin_password=$wp_admin_psswd \
+                --admin_email=$wp_admin_email \
+                --url=$db_host \
+                --allow-root
+
 wp user create $wp_user $wp_email --role=author --user_pass=$wp_psswd --allow-root
 
 sed -i "s/listen = \/run\/php\/php$name-fpm.sock/listen = 9000/1" /etc/php/$name/fpm/pool.d/www.conf
